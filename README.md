029b35edf7e9febf65be4574995abdbdba170b22
Krishna <77486234+codewithkrishnax@users.noreply.github.com>
2025-10-31 12:05:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # 📱 Ionic Mobile Emulator (Electron)
 
 A lightweight custom **Electron-based mobile emulator** designed for **Ionic developers**.  
@@ -21,3 +22,25 @@ It simulates a realistic mobile device frame and automatically loads your local 
 ```bash
 ionic serve
 ```
+=======
+# 📱 Ionic Mobile Emulator (Electron)
+
+A lightweight custom **Electron-based mobile emulator** designed for **Ionic developers**.  
+It simulates a realistic mobile device frame and automatically loads your local app from `http://localhost:8100` (via `ionic serve`).
+
+---
+
+## ⚙️ Features
+✅ Realistic mobile frame using PNG overlay  
+✅ Live reload support via `ionic serve`  
+✅ Custom `.bat` launcher for quick command access  
+✅ Clean and modern UI  
+
+---
+
+## 🚀 Usage
+
+### 1️⃣ Start your Ionic project
+```bash
+ionic serve
+>>>>>>> e00af9d (Initial commit)
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { app, BrowserWindow } from "electron";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -34,3 +35,40 @@ app.whenReady().then(() => {
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") app.quit();
 });
+=======
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let win;
+
+app.whenReady().then(() => {
+    win = new BrowserWindow({
+        width: 430,
+        height: 680,
+        transparent: true, // ✅ Transparent window
+        frame: false,      // ✅ No title bar / border
+        resizable: true,
+        movable:true,
+        backgroundColor: "#00000000",
+        center: true,
+        webPreferences: {
+            webviewTag: true,         // ✅ enable <webview>
+            contextIsolation: true,
+            sandbox: false
+        },
+    });
+
+    win.loadFile("index.html");
+    // win.webContents.openDevTools(); // optional
+
+    win.setMenuBarVisibility(false);
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
+>>>>>>> e00af9d (Initial commit)
